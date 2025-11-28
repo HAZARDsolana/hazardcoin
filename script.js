90cf08488bf39ee37bb64a27384b69ac9331e3f5
HAZARDsolana <vollmer.michael74@gmail.com>
2025-11-28 14:58:25 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('mousemove', (e) => {
-    const skull = document.querySelector('.skull');
-    const x = (window.innerWidth / 2 - e.pageX) / 40;
-    const y = (window.innerHeight / 2 - e.pageY) / 40;
-    skull.style.transform = `translate(${x}px, ${y}px)`;
-});
